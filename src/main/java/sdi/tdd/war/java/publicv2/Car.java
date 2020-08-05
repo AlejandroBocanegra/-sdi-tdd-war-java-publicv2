60ec9b2d863b87cab088fac76c1d185a3aa21435
AlejandroBocanegra <a.boca@outlook.com>
2020-08-06 06:31:57 +0900

    set up the files for initial state

---
+++ src/main/java/sdi/tdd/war/java/publicv2/Car.java
@@ -0,0 +1,24 @@
+package sdi.tdd.war.java.publicv2;
+
+import java.util.ArrayList;
+
+public class Car {
+  int mpg;
+  int gallons;
+  int odometer;
+  ArrayList<String> trips = new ArrayList<String>();
+
+  //Constructor for Car
+  public Car(int mpg){
+
+  }
+
+  public void fill(int gas){
+
+  }
+
+  public void drive(int miles){
+
+  }
+
+}
---
+++ src/test/java/sdi/tdd/war/java/publicv2/CarTest.java
@@ -0,0 +1,29 @@
+package sdi.tdd.war.java.publicv2;
+
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    //convert the javascript tests into these Java Tests----------
+    
+    @Test
+    public void ShouldFillWithGas() {
+      
+    }
+  
+    @Test
+    public void ShouldUseGasWhenDriving() {
+      
+    }
+  
+    @Test
+    public void ShouldIncrementOdometerWhenDriving() {
+     
+    }
+  
+    @Test
+    public void ShouldRecordTripsWhenDriving() {
+      
+    }
+  
+  }
